652393ddb195f1b605b5dbc48e860cc3accfecd3
agbomonica <agbomonica.am@gmail.com>
2021-10-21 03:20:55 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const navBtnEl = document.querySelector(".btn-mobile-nav");
+navBtnEl.addEventListener("click", function () {
+  document.body.classList.toggle("nav-open");
+});
+
+// Smooth Scrolling
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      document.body.classList.toggle("nav-open");
+    }
+  });
+});
